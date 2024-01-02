4850e5b05a14022817d18b5870439ee76780b137
MSI\dario <dariodisalvo94@hotmail.com>
2024-01-02 13:29:47 +0100

    Added card class

---
+++ main.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0, '/Users/dario/Desktop/Python Course/CardGame/CardGame/classes')
+from classes.Card import Card
+
+two_hearts = Card("Hearts", "Two")
+print(two_hearts)
